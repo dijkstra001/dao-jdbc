41868a561519cf7cfc53421173025edc781b8ac4
Nathã Correia <ncorreiatrabalhando@gmail.com>
2021-01-07 20:54:01 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,38 +10,51 @@ import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
-	
+
 	SimpleDateFormat sdf = new SimpleDateFormat("dd/MMMM/yyyy");
 
 	public static void main(String[] args) {
-		
-		SellerDao  sd = DaoFactory.createSellerDao();
+
+		SellerDao sd = DaoFactory.createSellerDao();
 		System.out.println("\n=== TEST 1: Seller FindById ===");
 		Seller seller = sd.findById(3);
-		
+
 		System.out.println(seller);
-		
+
 		System.out.println("\n=== TEST 2: Seller FindByDepartment ===");
 		Department department = new Department(2, null);
 		List<Seller> list = sd.findByDepartment(department);
-		
+
 		for (Seller s : list) {
 			System.out.println(s);
 		}
-		
+
 		System.out.println("\n=== TEST 3: Seller FindAll ===");
 		list = sd.findAll();
-		
+
 		for (Seller s : list) {
 			System.out.println(s);
 		}
-		
+
 		System.out.println("\n=== TEST 4: Seller Insert ===");
-		
+
 		Seller newSeller = new Seller(null, "Nomad", "nomad@gmail.com", new Date(), 3500.0, department);
-		
+
 		sd.insert(newSeller);
 		System.out.println("Inserted! New id " + newSeller.getId());
+
+		System.out.println("\n=== TEST 5: Seller Update ===");
+
+		seller = sd.findById(1);
+		seller.setName("Marta Waine");
+		sd.update(seller);
+
+		list = sd.findAll();
+
+		for (Seller s : list) {
+			System.out.println(s);
+		}
+
 	}
 
 }
