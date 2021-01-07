3bcfe0569310cf16d284b2f67a430735f6f463eb
Nathã Correia <ncorreiatrabalhando@gmail.com>
2021-01-07 07:58:50 -0300

    Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,13 @@
 package application;
 
-import java.sql.Connection;
-
-import db.DB;
+import model.entities.Department;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Connection connection = DB.getConnection();
-		DB.closeConnection();
+		Department dep = new Department(1, "Books");
+		System.out.println(dep);
 
 	}
 
