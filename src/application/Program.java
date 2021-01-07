e0097f5c74fc66e80a0f4022dcf2011eee6fc089
Nathã Correia <ncorreiatrabalhando@gmail.com>
2021-01-07 08:09:19 -0300

    Created Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,23 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Program {
+	
+	SimpleDateFormat sdf = new SimpleDateFormat("dd/MMMM/yyyy");
 
 	public static void main(String[] args) {
 		
 		Department dep = new Department(1, "Books");
 		System.out.println(dep);
-
+		
+		Seller sel = new Seller(1, "Bob", "bob@email.com", new Date(), 3000.0, dep);
+		System.out.println(sel);
+		
 	}
 
 }
